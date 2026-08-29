1cab24b507ae6946ba7e383c7138f4b22642f2c5
newgit6764 <layerextension@gmail.com>
2026-08-29 11:58:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,7 +178,7 @@ async function sendImageToTelegram(imageUrl, caption) {
         return;
     }
     try {
-        const url = `https://telegram.org{TELEGRAM_BOT_TOKEN}/sendPhoto`;
+        const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -205,7 +205,7 @@ async function sendToTelegram(text) {
         return;
     }
     try {
-        const url = `https://telegram.org{TELEGRAM_BOT_TOKEN}/sendMessage`;
+        const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
